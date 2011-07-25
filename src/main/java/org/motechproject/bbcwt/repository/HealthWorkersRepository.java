42ee76b2897300cfd4267402e2edd3a66e78d942
Raja Reddy <klnrajareddy@gmail.com>
2011-07-25 18:06:04 +0530

    Story 222|Introduced by_callerId view to HealthWorker.<Raja/Sri>

--- src/main/java/org/motechproject/bbcwt/repository/HealthWorkersRepository.java
+++ src/main/java/org/motechproject/bbcwt/repository/HealthWorkersRepository.java
@@ -2,10 +2,13 @@ package org.motechproject.bbcwt.repository;
 
 import org.ektorp.CouchDbConnector;
 import org.ektorp.support.CouchDbRepositorySupport;
+import org.ektorp.support.GenerateView;
 import org.motechproject.bbcwt.domain.HealthWorker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class HealthWorkersRepository extends CouchDbRepositorySupport<HealthWorker> {
@@ -15,5 +18,12 @@ public class HealthWorkersRepository extends CouchDbRepositorySupport<HealthWork
         initStandardDesignDocument();
     }
 
-
+    @GenerateView
+    public HealthWorker findByCallerId(String callerId) {
+        List<HealthWorker> healthWorkers = queryView("by_callerId", callerId);
+        if(healthWorkers!=null && !healthWorkers.isEmpty()) {
+            return healthWorkers.get(0);
+        }
+        return null;
+    }
 }
--- src/test/java/org/motechproject/bbcwt/repository/HealthWorkersRepositoryTest.java
+++ src/test/java/org/motechproject/bbcwt/repository/HealthWorkersRepositoryTest.java
@@ -1,5 +1,8 @@
 package org.motechproject.bbcwt.repository;
 
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.motechproject.bbcwt.domain.HealthWorker;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,22 +11,33 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
 public class HealthWorkersRepositoryTest extends SpringIntegrationTest {
-
     @Autowired
     private HealthWorkersRepository healthWorkersRepository;
 
+    private HealthWorker testHealthWorker;
+    private String callerId;
+
+    @Before
+    public void setUp() {
+        callerId = "9999988888";
+        testHealthWorker=new HealthWorker(callerId);
+        healthWorkersRepository.add(testHealthWorker);
+        markForDeletion(testHealthWorker);
+    }
+
     @Test
     public void testShouldPersistHealthWorker() {
-        String callerId = "9999988888";
-        HealthWorker testHealthWorker = new HealthWorker(callerId);
-        healthWorkersRepository.add(testHealthWorker);
-
         HealthWorker healthWorker = healthWorkersRepository.get(testHealthWorker.getId());
 
         assertNotNull(healthWorker);
         assertEquals("Caller Id should have been saved.", callerId, healthWorker.getCallerId());
-
-        markForDeletion(healthWorker);
     }
 
+    @Test
+    public void testShouldReturnHealthWorkerForAGivenCallerId() {
+        HealthWorker healthWorker = healthWorkersRepository.findByCallerId(callerId);
+
+        assertNotNull(healthWorker);
+        assertEquals("Caller Id should be the same as what has been queried for.", callerId, healthWorker.getCallerId());
+    }
 }
