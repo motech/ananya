46710157f0e317e9179a608d141f6317b4d41cc4
Raja Reddy <klnrajareddy@gmail.com>
2011-07-29 14:15:12 +0530

    Story 226|Setting up seed data for chapters.<Raja/Sri>

---
+++ src/main/java/org/motechproject/bbcwt/tools/CouchDB.java
@@ -0,0 +1,19 @@
+package org.motechproject.bbcwt.tools;
+
+import org.ektorp.CouchDbConnector;
+import org.ektorp.CouchDbInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CouchDB {
+    @Autowired
+    private CouchDbInstance couchDbInstance;
+    @Autowired
+    private CouchDbConnector tamaDbConnector;
+
+    public void recreate() {
+        String dbName = tamaDbConnector.getDatabaseName();
+        couchDbInstance.deleteDatabase(dbName);
+        couchDbInstance.createDatabase(dbName);
+    }
+}
+
---
+++ src/main/java/org/motechproject/bbcwt/tools/seed/ChapterSeed.java
@@ -0,0 +1,35 @@
+package org.motechproject.bbcwt.tools.seed;
+
+import org.motechproject.bbcwt.domain.Chapter;
+import org.motechproject.bbcwt.domain.Lesson;
+import org.motechproject.bbcwt.repository.ChaptersRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChapterSeed extends Seed {
+    @Autowired
+    private ChaptersRespository chapters;
+
+
+    @Override
+    protected void load() {
+        Chapter chapter1 = new Chapter(1);
+        Lesson lesson1Ch1 = new Lesson(1, "You are hearing to lesson 1 in chapter 1.");
+        Lesson lesson2Ch1 = new Lesson(2, "You are hearing to lesson 2 in chapter 1.");
+
+        chapter1.addLesson(lesson1Ch1);
+        chapter1.addLesson(lesson2Ch1);
+
+        chapters.add(chapter1);
+
+        Chapter chapter2 = new Chapter(1);
+        Lesson lesson1Ch2 = new Lesson(1, "You are hearing to lesson 1 in chapter 2.");
+        Lesson lesson2Ch2 = new Lesson(2, "You are hearing to lesson 2 in chapter 2.");
+
+        chapter2.addLesson(lesson1Ch2);
+        chapter2.addLesson(lesson2Ch2);
+
+        chapters.add(chapter2);
+    }
+}
