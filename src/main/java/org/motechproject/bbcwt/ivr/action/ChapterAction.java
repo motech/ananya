62def20de1fe074ba5c2974def6b6cce6a3117fa
Raja Reddy <klnrajareddy@gmail.com>
2011-08-02 11:29:26 +0530

    Story 226|Implemented checkpointing.

--- src/main/java/org/motechproject/bbcwt/ivr/action/ChapterAction.java
+++ src/main/java/org/motechproject/bbcwt/ivr/action/ChapterAction.java
@@ -2,10 +2,17 @@ package org.motechproject.bbcwt.ivr.action;
 
 
 import org.motechproject.bbcwt.domain.Chapter;
+import org.motechproject.bbcwt.domain.HealthWorker;
 import org.motechproject.bbcwt.domain.Lesson;
+import org.motechproject.bbcwt.domain.Milestone;
+import org.motechproject.bbcwt.ivr.IVR;
 import org.motechproject.bbcwt.ivr.IVRMessage;
 import org.motechproject.bbcwt.ivr.IVRRequest;
 import org.motechproject.bbcwt.repository.ChaptersRespository;
+import org.motechproject.bbcwt.repository.HealthWorkersRepository;
+import org.motechproject.bbcwt.repository.MilestonesRepository;
+import org.motechproject.bbcwt.util.DateUtil;
+import org.openqa.selenium.remote.server.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,28 +22,44 @@ import sun.rmi.runtime.Log;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 @Controller
 public class ChapterAction extends BaseAction {
     private ChaptersRespository chaptersRespository;
+    private MilestonesRepository milestonesRepository;
+    private HealthWorkersRepository healthWorkersRepository;
+    private DateUtil dateUtil;
 
     @Autowired
-    public ChapterAction(ChaptersRespository chaptersRespository, IVRMessage messages)  {
+    public ChapterAction(HealthWorkersRepository healthWorkersRepository, ChaptersRespository chaptersRespository, MilestonesRepository milestonesRepository, DateUtil dateUtil, IVRMessage messages)  {
+        this.healthWorkersRepository = healthWorkersRepository;
         this.chaptersRespository = chaptersRespository;
+        this.milestonesRepository = milestonesRepository;
         this.messages = messages;
+        this.dateUtil = dateUtil;
     }
 
     @RequestMapping(value="/chapter/{chapterNumber}/lesson/{lessonNumber}", method= RequestMethod.GET)
     public String get(@PathVariable("chapterNumber") int chapterNumber, @PathVariable("lessonNumber") int lessonNumber, HttpServletRequest request, HttpServletResponse response) {
-        LOG.info("Incoming request to play chapter: " + chapterNumber + "'s lesson: " + lessonNumber + ".");
-
         Chapter chapter = chaptersRespository.findByNumber(chapterNumber);
-        LOG.info("Fetched chapter: " + chapter);
         Lesson lessonToPlay = chapter.getLessonByNumber(lessonNumber);
 
-        LOG.info("Should be playing content at: " + lessonToPlay.getLocation());
+        String callerId = (String)request.getSession().getAttribute(IVR.Attributes.CALLER_ID);
+        HealthWorker healthWorker = healthWorkersRepository.findByCallerId(callerId);
+        Milestone milestone = milestonesRepository.findByHealthWorker(healthWorker);
+
+        if(milestone == null) {
+            milestone = new Milestone();
+        }
+
+        milestone.setChapterId(chapter.getId());
+        milestone.setLessonId(lessonToPlay.getId());
+        milestone.setStartDate(dateUtil.getDate());
+        milestone.setHealthWorkerId(healthWorker.getId());
+        milestonesRepository.add(milestone);
+
         ivrResponseBuilder(request).addPlayText(lessonToPlay.getLocation());
-        LOG.info("Forwarding to helpmenu.");
         return "forward:/helpMenu";
     }
 
