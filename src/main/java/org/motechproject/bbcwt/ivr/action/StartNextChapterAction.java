cd093f3ac24084ef72e799923d5dd41c6278a3cb
Raja Reddy <klnrajareddy@gmail.com>
2011-08-09 17:23:25 +0530

    Story 227|Goto next chapter action.

---
+++ src/main/java/org/motechproject/bbcwt/ivr/action/StartNextChapterAction.java
@@ -0,0 +1,49 @@
+package org.motechproject.bbcwt.ivr.action;
+
+import org.motechproject.bbcwt.domain.Chapter;
+import org.motechproject.bbcwt.domain.Milestone;
+import org.motechproject.bbcwt.ivr.IVR;
+import org.motechproject.bbcwt.ivr.IVRMessage;
+import org.motechproject.bbcwt.ivr.IVRRequest;
+import org.motechproject.bbcwt.repository.ChaptersRespository;
+import org.motechproject.bbcwt.repository.MilestonesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/startNextChapter")
+public class StartNextChapterAction extends BaseAction {
+    private MilestonesRepository milestonesRepository;
+    private ChaptersRespository chaptersRespository;
+
+    @Autowired
+    public StartNextChapterAction(MilestonesRepository milestonesRepository, ChaptersRespository chaptersRespository, IVRMessage messages) {
+        this.milestonesRepository = milestonesRepository;
+        this.chaptersRespository = chaptersRespository;
+        this.messages = messages;
+    }
+
+    @Override
+    @RequestMapping(method= RequestMethod.GET)
+    public String handle(IVRRequest ivrRequest, HttpServletRequest request, HttpServletResponse response) {
+        String healthWorkerCallerId = (String)request.getSession().getAttribute(IVR.Attributes.CALLER_ID);
+
+        Milestone currentMilestone = milestonesRepository.currentMilestoneWithLinkedReferences(healthWorkerCallerId);
+        Chapter currentChapter = currentMilestone.getChapter();
+        int currentChapterNumber = currentChapter.getNumber();
+
+        int nextChapterNumber = currentChapterNumber + 1;
+        Chapter nextChapter = chaptersRespository.findByNumber(nextChapterNumber);
+
+        if(nextChapter == null) {
+            return "forward:/endOfCourse";
+        }
+
+        return "forward:/chapter/"+ nextChapterNumber +"/lesson/1";
+    }
+}
