db41ec97a88dd32a2aeb2be8ee3f7587cfe0d6dd
Raja Reddy <klnrajareddy@gmail.com>
2011-08-09 17:34:14 +0530

    Story 227|Goto next last chapter action.

---
+++ src/main/java/org/motechproject/bbcwt/ivr/action/RepeatLastChapterAction.java
@@ -0,0 +1,39 @@
+package org.motechproject.bbcwt.ivr.action;
+
+import org.motechproject.bbcwt.domain.Chapter;
+import org.motechproject.bbcwt.domain.Milestone;
+import org.motechproject.bbcwt.ivr.IVR;
+import org.motechproject.bbcwt.ivr.IVRMessage;
+import org.motechproject.bbcwt.ivr.IVRRequest;
+import org.motechproject.bbcwt.repository.MilestonesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/repeatLastChapter")
+public class RepeatLastChapterAction extends BaseAction {
+    private MilestonesRepository milestonesRepository;
+
+    @Autowired
+    public RepeatLastChapterAction(MilestonesRepository milestonesRepository, IVRMessage messages) {
+        this.milestonesRepository = milestonesRepository;
+        this.messages = messages;
+    }
+
+    @Override
+    @RequestMapping(method= RequestMethod.GET)
+    public String handle(IVRRequest ivrRequest, HttpServletRequest request, HttpServletResponse response) {
+        String healthWorkerCallerId = (String)request.getSession().getAttribute(IVR.Attributes.CALLER_ID);
+
+        Milestone currentMilestone = milestonesRepository.currentMilestoneWithLinkedReferences(healthWorkerCallerId);
+        Chapter currentChapter = currentMilestone.getChapter();
+        int currentChapterNumber = currentChapter.getNumber();
+
+        return "forward:/chapter/"+ currentChapterNumber +"/lesson/1";
+    }
+}
