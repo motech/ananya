82c1afb5b8f9f52d528ffce88356d15e09820132
Raja Reddy <klnrajareddy@gmail.com>
2011-08-22 16:19:32 +0530

    Story 239|Making a utility class to delete milestones history.

---
+++ src/main/java/org/motechproject/bbcwt/tools/maintenance/DeleteMilestonesHistory.java
@@ -0,0 +1,35 @@
+package org.motechproject.bbcwt.tools.maintenance;
+
+import org.ektorp.BulkDeleteDocument;
+import org.ektorp.CouchDbConnector;
+import org.motechproject.bbcwt.domain.HealthWorker;
+import org.motechproject.bbcwt.domain.Milestone;
+import org.motechproject.bbcwt.repository.HealthWorkersRepository;
+import org.motechproject.bbcwt.repository.MilestonesRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeleteMilestonesHistory {
+    public static final String APPLICATION_CONTEXT_XML = "META-INF/spring/applicationContext-tools.xml";
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML);
+
+        CouchDbConnector db = context.getBean(CouchDbConnector.class);
+
+        HealthWorkersRepository healthWorkers = context.getBean(HealthWorkersRepository.class);
+        MilestonesRepository milestones = context.getBean(MilestonesRepository.class);
+
+        List<BulkDeleteDocument> docsToDelete = new ArrayList<BulkDeleteDocument>();
+        for(Milestone eachMilestone : milestones.getAll()) {
+            HealthWorker healthWorker = healthWorkers.get(eachMilestone.getHealthWorkerId());
+            docsToDelete.add(BulkDeleteDocument.of(healthWorker));
+            docsToDelete.add(BulkDeleteDocument.of(eachMilestone));
+        }
+
+        db.executeBulk(docsToDelete);
+    }
+}
