72a1e7ef194e571c797737ed7f93868e1c95f96a
Raja Reddy <klnrajareddy@gmail.com>
2011-09-08 21:22:12 +0530

    Refactored EndOfQuizMenuAnswerAction

--- src/main/java/org/motechproject/bbcwt/ivr/action/EndOfQuizMenuAnswerAction.java
+++ src/main/java/org/motechproject/bbcwt/ivr/action/EndOfQuizMenuAnswerAction.java
@@ -1,7 +1,10 @@
 package org.motechproject.bbcwt.ivr.action;
 
+import org.motechproject.bbcwt.ivr.IVRContext;
 import org.motechproject.bbcwt.ivr.IVRMessage;
 import org.motechproject.bbcwt.ivr.IVRRequest;
+import org.motechproject.bbcwt.ivr.action.inputhandler.KeyPressHandler;
+import org.motechproject.bbcwt.ivr.builder.IVRResponseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,40 +12,87 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/endOfQuizMenuAnswer")
 public class EndOfQuizMenuAnswerAction extends BaseAction {
-    private static final Map<Character, String> INPUT_FORWARD_MAP;
-
-    static {
-        INPUT_FORWARD_MAP = new HashMap<Character, String>();
-        INPUT_FORWARD_MAP.put('1', "forward:/startQuiz");
-        INPUT_FORWARD_MAP.put('2', "forward:/startNextChapter");
-        INPUT_FORWARD_MAP.put('3', "forward:/repeatLastChapter");
-    }
+    private Map<Character, KeyPressHandler> keyPressHandlerMap;
+    private KeyPressHandler invalidKeyPressHandler;
 
     @Autowired
     public EndOfQuizMenuAnswerAction(IVRMessage messages) {
         this.messages = messages;
+        intializeKeyPressHandlerMap();
+        invalidKeyPressHandler = new InvalidInputHandler();
+    }
+
+    private void intializeKeyPressHandlerMap() {
+        keyPressHandlerMap = new HashMap<Character, KeyPressHandler>();
+        keyPressHandlerMap.put('1', new StartQuizHandler());
+        keyPressHandlerMap.put('2', new StartNextChapterHandler());
+        keyPressHandlerMap.put('3', new RepeatLastChapterHandler());
+        keyPressHandlerMap.put(NO_INPUT, new NoInputHandler());
     }
 
     @Override
     @RequestMapping(method = RequestMethod.GET)
     public String handle(IVRRequest ivrRequest, HttpServletRequest request, HttpServletResponse response) {
-        char ivrInput = ivrInput(ivrRequest);
-        if(inputInvalid(ivrInput)) {
-            if(ivrInput != NO_INPUT) {
-                ivrResponseBuilder(request).addPlayAudio(absoluteFileLocation(messages.get(IVRMessage.INVALID_INPUT)));
-            }
-            return "forward:/endOfQuizMenu";
-        }
-        return INPUT_FORWARD_MAP.get(ivrInput);
+        HttpSession session = request.getSession();
+
+        IVRContext.SessionAndIVRContextSynchronizer synchronizer = new IVRContext.SessionAndIVRContextSynchronizer();
+        IVRContext ivrContext = synchronizer.buildIVRContext(session);
+
+        char chosenOption = ivrInput(ivrRequest);
+        KeyPressHandler keyPressResponseAction = determineActionToExecute(chosenOption);
+
+        String forward = keyPressResponseAction.execute(chosenOption, ivrContext, ivrResponseBuilder(request));
+
+        synchronizer.synchronizeSessionWithIVRContext(session, ivrContext);
+
+        return forward;
     }
 
-    private boolean inputInvalid(char chosenOption) {
-        return !INPUT_FORWARD_MAP.containsKey(chosenOption);
+    private KeyPressHandler determineActionToExecute(char chosenOption) {
+        KeyPressHandler responseAction = keyPressHandlerMap.get(chosenOption);
+        return responseAction!=null?responseAction: invalidKeyPressHandler;
+    }
+
+    private class StartQuizHandler implements KeyPressHandler {
+        @Override
+        public String execute(Character keyPressed, IVRContext ivrContext, IVRResponseBuilder ivrResponseBuilder) {
+            return "forward:/startQuiz";
+        }
+    }
+
+    private class StartNextChapterHandler implements KeyPressHandler {
+        @Override
+        public String execute(Character keyPressed, IVRContext ivrContext, IVRResponseBuilder ivrResponseBuilder) {
+            return "forward:/startNextChapter";
+        }
+    }
+
+    private class RepeatLastChapterHandler implements KeyPressHandler {
+        @Override
+        public String execute(Character keyPressed, IVRContext ivrContext, IVRResponseBuilder ivrResponseBuilder) {
+            return "forward:/repeatLastChapter";
+        }
+    }
+
+    private class NoInputHandler implements KeyPressHandler {
+        @Override
+        public String execute(Character keyPressed, IVRContext ivrContext, IVRResponseBuilder ivrResponseBuilder) {
+            return "forward:/endOfQuizMenu";
+        }
+    }
+
+    private class InvalidInputHandler implements KeyPressHandler {
+        @Override
+        public String execute(Character keyPressed, IVRContext ivrContext, IVRResponseBuilder ivrResponseBuilder) {
+            ivrResponseBuilder.addPlayAudio(absoluteFileLocation(messages.get(IVRMessage.INVALID_INPUT)));
+            return "forward:/endOfQuizMenu";
+        }
     }
 }
