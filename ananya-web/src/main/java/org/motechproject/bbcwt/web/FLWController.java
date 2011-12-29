515848b02556783e9a32a2fdbba01057fca82c0f
vijayaraj <vijayaraj@yahoo.com>
2011-12-29 16:04:06 +0530

    sushmita, vijay | #976 | velocity wiring complete

--- ananya-web/src/main/java/org/motechproject/bbcwt/web/FLWController.java
+++ ananya-web/src/main/java/org/motechproject/bbcwt/web/FLWController.java
@@ -4,16 +4,17 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Scope(value = "prototype")
 public class FLWController {
 
+    private static final String PAGE = "some";
+
     @RequestMapping(method = {RequestMethod.GET}, value = "/status")
-    @ResponseBody
-    public String callFlow() {
-        return "Hey hey";
+    public ModelAndView callFlow() {
+       return new ModelAndView(PAGE);
     }
 
 
