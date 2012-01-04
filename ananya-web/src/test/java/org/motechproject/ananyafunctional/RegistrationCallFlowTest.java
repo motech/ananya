79ce61e1b70fe2ae275ae50727ed0ced21279d16
vijayaraj <vijayaraj@yahoo.com>
2012-01-04 09:16:36 +0530

    priyanka, vijay | #976 | functional test for reg call flow

--- ananya-web/src/test/java/org/motechproject/ananyafunctional/RegistrationCallFlowTest.java
+++ ananya-web/src/test/java/org/motechproject/ananyafunctional/RegistrationCallFlowTest.java
@@ -2,21 +2,43 @@ package org.motechproject.ananyafunctional;
 
 import org.junit.Test;
 import org.motechproject.ananya.repository.AllFrontLineWorkers;
-import org.motechproject.ananyafunctional.framework.BaseCallFlowTest;
 import org.motechproject.ananyafunctional.framework.CallFlow;
 import org.motechproject.ananyafunctional.framework.MyWebClient;
+import org.motechproject.bbcwt.repository.SpringIntegrationTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.w3c.dom.NodeList;
 
-public class RegistrationCallFlowTest extends BaseCallFlowTest {
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+
+import static java.util.Arrays.asList;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+public class RegistrationCallFlowTest extends SpringIntegrationTest {
 
     @Autowired
     private AllFrontLineWorkers allFrontLineWorkers;
 
     @Test
     public void shouldGetARegistrationVxmlForAUnRegisteredFLW() throws Exception {
+
         MyWebClient myWebClient = new MyWebClient();
         CallFlow callFlow = myWebClient.getCallFlow("http://localhost:9979/ananya/flw/vxml/?session.callerid=123");
-        String vxml = callFlow.vxml();
-        assertXpathExists("//prompt", transform(vxml));
+
+        for (String record : asList("name", "district", "block", "village"))
+            assertOnRecordElement(callFlow, record);
+
+        NodeList read = (NodeList) callFlow.read("/vxml/form/var[@name='msisdn']", XPathConstants.NODESET);
+        assertEquals("session.callerid", read.item(0).getAttributes().item(0).getTextContent());
     }
+
+    private void assertOnRecordElement(CallFlow callFlow, String param) throws XPathExpressionException {
+        assertNotNull(callFlow.read("/vxml/form/record[@name='" + param + "']/noinput/prompt", XPathConstants.NODESET));
+        assertNotNull(callFlow.read("/vxml/form/record[@name='" + param + "']/prompt", XPathConstants.NODESET));
+        assertNotNull(callFlow.read("/vxml/form/record[@name='" + param + "']/grammer", XPathConstants.NODESET));
+        assertNotNull(callFlow.read("/vxml/form/record[@name='" + param + "']/filled/prompt", XPathConstants.NODESET));
+    }
+
+
 }
--- ananya-web/src/test/java/org/motechproject/ananyafunctional/framework/CallFlow.java
+++ ananya-web/src/test/java/org/motechproject/ananyafunctional/framework/CallFlow.java
@@ -1,10 +1,43 @@
 package org.motechproject.ananyafunctional.framework;
 
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.StringReader;
+
 public class CallFlow {
+    private Document doc;
+    private XPath xPath;
     private String vxml;
 
     public CallFlow(String vxml) {
         this.vxml = vxml;
+        init();
+    }
+
+    private void init() {
+        try {
+            DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            InputSource is = new InputSource();
+            is.setCharacterStream(new StringReader(vxml));
+            this.doc = db.parse(is);
+
+            xPath = XPathFactory.newInstance().newXPath();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public Object read(String expression, QName returnType) throws XPathExpressionException {
+        XPathExpression xPathExpression = xPath.compile(expression);
+        return xPathExpression.evaluate(doc, returnType);
     }
 
     public String vxml() {
