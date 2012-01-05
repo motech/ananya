3ff8a6cee7bd07b97f1c96a97d5643a2aa084307
sushmitharaos <sushmitharaos@gmail.com>
2012-01-05 17:37:55 +0530

    Priyanka,Sushmitha|#983|Added functional tests for landing page.

--- ananya-web/src/test/java/org/motechproject/ananyafunctional/RegistrationCallFlowTest.java
+++ ananya-web/src/test/java/org/motechproject/ananyafunctional/RegistrationCallFlowTest.java
@@ -1,7 +1,8 @@
 package org.motechproject.ananyafunctional;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.motechproject.ananya.domain.FrontLineWorker;
+import org.motechproject.ananya.domain.FrontLineWorkerStatus;
 import org.motechproject.ananya.repository.AllFrontLineWorkers;
 import org.motechproject.ananyafunctional.framework.CallFlow;
 import org.motechproject.ananyafunctional.framework.MyWebClient;
@@ -21,17 +22,36 @@ public class RegistrationCallFlowTest extends SpringIntegrationTest {
     @Autowired
     private AllFrontLineWorkers allFrontLineWorkers;
 
-    @Test@Ignore
+    @Test
     public void shouldGetARegistrationVxmlForAUnRegisteredFLW() throws Exception {
 
         MyWebClient myWebClient = new MyWebClient();
-        CallFlow callFlow = myWebClient.getCallFlow("http://localhost:9979/ananya/flw/vxml/?session.callerid=123");
+        CallFlow callFlow = myWebClient.getCallFlow("http://localhost:9979/ananya/vxml/register/?session.callerid=321");
 
         for (String record : asList("name", "district", "block", "village"))
             assertOnRecordElement(callFlow, record);
 
         NodeList read = (NodeList) callFlow.read("/vxml/form/var[@name='msisdn']", XPathConstants.NODESET);
-//        assertEquals("session.callerid", read.item(0).getAttributes().item(0).getTextContent());
+        assertEquals("session.callerid", read.item(0).getAttributes().item(0).getTextContent());
+    }
+
+    @Test
+    public void shouldGetLandingPageWithRegistrationUrlForUnregisteredFLW() throws Exception {
+        MyWebClient myWebClient = new MyWebClient();
+        CallFlow callFlow = myWebClient.getCallFlow("http://localhost:9979/ananya/vxml/landing/?session.callerid=123");
+        NodeList read = (NodeList) callFlow.read("/vxml/form/block/goto", XPathConstants.NODESET);
+        assertEquals("/ananya/vxml/register/", read.item(0).getAttributes().item(0).getTextContent());
+    }
+
+    @Test
+    public void shouldGetLandingPageWithMenuUrlForRegisteredFLW() throws Exception {
+        MyWebClient myWebClient = new MyWebClient();
+        FrontLineWorker flw = new FrontLineWorker("123").status(FrontLineWorkerStatus.REGISTERED);
+        allFrontLineWorkers.add(flw);
+        markForDeletion(flw);
+        CallFlow callFlow = myWebClient.getCallFlow("http://localhost:9979/ananya/vxml/landing/?session.callerid=123");
+        NodeList read = (NodeList) callFlow.read("/vxml/form/block/goto", XPathConstants.NODESET);
+        assertEquals("/ananya/vxml/menu/", read.item(0).getAttributes().item(0).getTextContent());
     }
 
     private void assertOnRecordElement(CallFlow callFlow, String param) throws XPathExpressionException {
@@ -40,6 +60,4 @@ public class RegistrationCallFlowTest extends SpringIntegrationTest {
         assertNotNull(callFlow.read("/vxml/form/record[@name='" + param + "']/grammer", XPathConstants.NODESET));
         assertNotNull(callFlow.read("/vxml/form/record[@name='" + param + "']/filled/prompt", XPathConstants.NODESET));
     }
-
-
 }
