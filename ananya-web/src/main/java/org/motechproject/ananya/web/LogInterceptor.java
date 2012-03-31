322fc239fb39b3c7434122e3dad5a38da4d22c64
vijayaraj <vijayaraj@yahoo.com>
2012-03-31 12:38:30 +0530

    vijay|#00|made log a single line in http log interceptor

--- ananya-web/src/main/java/org/motechproject/ananya/web/LogInterceptor.java
+++ ananya-web/src/main/java/org/motechproject/ananya/web/LogInterceptor.java
@@ -8,30 +8,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Enumeration;
 
-public class LogInterceptor extends HandlerInterceptorAdapter{
+public class LogInterceptor extends HandlerInterceptorAdapter {
     private final static Logger LOG = LoggerFactory.getLogger(LogInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        LOG.info("Request START");
-        LOG.info("Request URI - " + request.getRequestURI());
-        LOG.info("Request parameters");
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Request START [uri=" + request.getRequestURI() + "|");
+        builder.append("Parameters={");
 
         Enumeration<String> requestKeys = request.getParameterNames();
         while (requestKeys.hasMoreElements()) {
             String key = requestKeys.nextElement();
-            LOG.info(key + "  -->  " + request.getParameter(key));
+            builder.append(key + "=>" + request.getParameter(key) + ",");
         }
-
-        //String callId = request.getParameter("callId");
+        builder.append("}]");
+        LOG.info(builder.toString());
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        LOG.info(request.getRequestURI());
-        // TODO: figure out how to dump response as a string in debug mode
-//        LOG.debug(response.getOutputStream().toString());
-        LOG.info("Request END");
+        LOG.info("Request END [uri=" + request.getRequestedSessionId());
     }
 }
