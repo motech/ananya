0904669eddb52020ca577f279a13e42a46499559
Sneha Koti <snehakot@thoughtworks.com>
2012-04-04 11:34:43 +0530

    Sneha,Aravind | #00 | Added sms.properties to ananya-service.

--- ananya-report/src/test/java/org/motechproject/ananya/service/integrationtests/SMSSentDataHandlerIT.java
+++ ananya-report/src/test/java/org/motechproject/ananya/service/integrationtests/SMSSentDataHandlerIT.java
@@ -5,9 +5,11 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.motechproject.ananya.SpringIntegrationTest;
-import org.motechproject.ananya.domain.*;
+import org.motechproject.ananya.domain.Designation;
+import org.motechproject.ananya.domain.FrontLineWorker;
+import org.motechproject.ananya.domain.Location;
+import org.motechproject.ananya.domain.RegistrationStatus;
 import org.motechproject.ananya.domain.dimension.FrontLineWorkerDimension;
-import org.motechproject.ananya.domain.dimension.TimeDimension;
 import org.motechproject.ananya.domain.measure.SMSSentMeasure;
 import org.motechproject.ananya.repository.AllFrontLineWorkers;
 import org.motechproject.ananya.repository.AllSMSReferences;
@@ -15,15 +17,23 @@ import org.motechproject.ananya.repository.dimension.AllFrontLineWorkerDimension
 import org.motechproject.ananya.repository.dimension.AllTimeDimensions;
 import org.motechproject.ananya.requests.LogData;
 import org.motechproject.ananya.requests.LogType;
+import org.motechproject.ananya.requests.ReportPublishEventKeys;
 import org.motechproject.ananya.service.handler.SMSSentDataHandler;
+import org.motechproject.context.Context;
 import org.motechproject.model.MotechEvent;
+import org.motechproject.server.event.EventListener;
+import org.motechproject.server.event.EventListenerRegistry;
+import org.motechproject.server.event.annotations.MotechListenerAbstractProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -53,6 +63,21 @@ public class SMSSentDataHandlerIT extends SpringIntegrationTest {
     }
 
     @Test
+    public void shouldBindToTheCorrectListener() throws NoSuchFieldException, IllegalAccessException {
+        EventListenerRegistry registry = Context.getInstance().getEventListenerRegistry();
+        Set<EventListener> listeners = registry.getListeners(ReportPublishEventKeys.SEND_SMS_SENT_DATA_KEY);
+
+        MotechListenerAbstractProxy motechListenerAbstractProxy = (MotechListenerAbstractProxy) listeners.toArray()[0];
+
+        Field declaredField = MotechListenerAbstractProxy.class.getDeclaredField("method");
+        declaredField.setAccessible(true);
+        Method handler = (Method) declaredField.get(motechListenerAbstractProxy);
+        assertEquals(1, listeners.size());
+        assertEquals(SMSSentDataHandler.class,handler.getDeclaringClass());
+        assertEquals("handleSMSSent",handler.getName());
+    }
+
+    @Test
     public void shouldLogSMSSentMeasure() {
         String msisdn = "9" + System.currentTimeMillis();
         FrontLineWorker flw = new FrontLineWorker(msisdn, "Rani",Designation.ANGANWADI, new Location(), RegistrationStatus.REGISTERED);
@@ -61,8 +86,8 @@ public class SMSSentDataHandlerIT extends SpringIntegrationTest {
         allFrontLineWorkers.add(flw);
 
         DateTime now = DateTime.now();
-        TimeDimension timeDimension = allTimeDimensions.addOrUpdate(now);
-        FrontLineWorkerDimension frontLineWorkerDimension = allFrontLineWorkerDimensions.getOrMakeFor(Long.valueOf(msisdn), "airtel", "Rani", "REGISTERED");
+        allTimeDimensions.addOrUpdate(now);
+        allFrontLineWorkerDimensions.getOrMakeFor(Long.valueOf(msisdn), "airtel", "Rani", "REGISTERED");
 
         LogData logData = new LogData(LogType.SMS_SENT, msisdn);
         Map<String, Object> map = new HashMap<String, Object>();
