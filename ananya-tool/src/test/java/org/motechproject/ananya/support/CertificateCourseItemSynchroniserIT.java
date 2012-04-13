6d17ca03847c23624de024af82827cb4e6d39848
vijayaraj <vijayaraj@yahoo.com>
2012-04-13 14:09:45 +0530

    ashok, vijay|#1535 | added IT for CertificateCourseItemSynchroniser

---
+++ ananya-tool/src/test/java/org/motechproject/ananya/support/CertificateCourseItemSynchroniserIT.java
@@ -0,0 +1,101 @@
+package org.motechproject.ananya.support;
+
+
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.motechproject.ananya.domain.*;
+import org.motechproject.ananya.domain.dimension.CourseItemDimension;
+import org.motechproject.ananya.domain.dimension.FrontLineWorkerDimension;
+import org.motechproject.ananya.domain.dimension.TimeDimension;
+import org.motechproject.ananya.domain.measure.CourseItemMeasure;
+import org.motechproject.ananya.repository.AllCertificateCourseLogs;
+import org.motechproject.ananya.repository.DataAccessTemplate;
+import org.motechproject.ananya.repository.dimension.AllCourseItemDimensions;
+import org.motechproject.ananya.repository.dimension.AllFrontLineWorkerDimensions;
+import org.motechproject.ananya.repository.dimension.AllTimeDimensions;
+import org.motechproject.ananya.support.log.SynchroniserLog;
+import org.motechproject.ananya.support.log.SynchroniserLogItem;
+import org.motechproject.ananya.support.synchroniser.CertificateCourseItemSynchroniser;
+import org.motechproject.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext-tool.xml")
+public class CertificateCourseItemSynchroniserIT {
+
+    @Autowired
+    private CertificateCourseItemSynchroniser certificateCourseItemSychroniser;
+
+    @Autowired
+    private AllCertificateCourseLogs allCertificateCourseLogs;
+
+    @Autowired
+    private DataAccessTemplate template;
+
+    @Autowired
+    private AllTimeDimensions allTimeDimensions;
+
+    @Autowired
+    private AllFrontLineWorkerDimensions allFrontLineWorkerDimensions;
+
+    @Autowired
+    private AllCourseItemDimensions allCourseItemDimensions;
+
+    @Before
+    public void setUp() {
+        template.deleteAll(template.loadAll(FrontLineWorkerDimension.class));
+        template.deleteAll(template.loadAll(TimeDimension.class));
+        template.deleteAll(template.loadAll(CourseItemDimension.class));
+        template.deleteAll(template.loadAll(CourseItemMeasure.class));
+        allCertificateCourseLogs.removeAll();
+    }
+
+    @Test
+    public void shouldMigrateCallDurationDataFromTransactionDBToReportDB() {
+        String callerId = "1234";
+        String callId = "1234-5678";
+        DateTime callStartTime = DateUtil.now();
+        String contentId = "contentId";
+        String contentName = "Chapter4";
+        CourseItemType courseItemType = CourseItemType.CHAPTER;
+
+        FrontLineWorkerDimension frontLineWorkerDimension = allFrontLineWorkerDimensions.getOrMakeFor(Long.valueOf(callerId), "airtel", "name", RegistrationStatus.PARTIALLY_REGISTERED.toString());
+        allTimeDimensions.addOrUpdate(callStartTime);
+        allCourseItemDimensions.add(new CourseItemDimension(contentName, contentId, courseItemType));
+
+        CertificationCourseLog certificationCourseLog = new CertificationCourseLog(callerId, "9909", callStartTime, callStartTime.plusSeconds(20), "airtel", callId, "1");
+        CertificationCourseLogItem courseLogItem = new CertificationCourseLogItem(contentId, courseItemType, contentName, "3", CourseItemState.START, DateUtil.now());
+        certificationCourseLog.addCourseLogItem(courseLogItem);
+        allCertificateCourseLogs.add(certificationCourseLog);
+
+        DateTime fromDate = DateUtil.now();
+        DateTime toDate = fromDate.plusHours(8);
+
+        SynchroniserLog synchroniserLog = certificateCourseItemSychroniser.replicate(fromDate, toDate);
+        List<CourseItemMeasure> courseItemMeasures = template.loadAll(CourseItemMeasure.class);
+
+        CourseItemMeasure courseItemMeasureFromDb = null;
+        for (CourseItemMeasure courseItemMeasure : courseItemMeasures)
+            if (courseItemMeasure.getCourseItemDimension().getContentId().equals(contentId))
+                courseItemMeasureFromDb = courseItemMeasure;
+
+        assertNotNull(courseItemMeasureFromDb);
+        assertThat(courseItemMeasureFromDb.getFrontLineWorkerDimension().getMsisdn(), is(frontLineWorkerDimension.getMsisdn()));
+
+
+        List<SynchroniserLogItem> synchroniserLogItems = synchroniserLog.getItems();
+        assertThat(synchroniserLogItems.size(), is(1));
+        assertThat(synchroniserLogItems.get(0).print(), is("1234-5678: Success"));
+    }
+
+}
