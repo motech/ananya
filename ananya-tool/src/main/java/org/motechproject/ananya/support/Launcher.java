7a5c41b654b180253ccb8d4168ba2d302aaf4985
vijayaraj <vijayaraj@yahoo.com>
2012-04-14 12:24:57 +0530

    vijay|#1535|fixed synch launcher

--- ananya-tool/src/main/java/org/motechproject/ananya/support/Launcher.java
+++ ananya-tool/src/main/java/org/motechproject/ananya/support/Launcher.java
@@ -1,24 +1,23 @@
 package org.motechproject.ananya.support;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.motechproject.ananya.support.synchroniser.AllSynchronisers;
-import org.motechproject.util.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.text.SimpleDateFormat;
 
 public class Launcher {
 
     private static Logger log = LoggerFactory.getLogger(Launcher.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         log.info("Synchronising data: START:");
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("mm");
-        DateTime fromDate = DateUtil.now();
-        DateTime toDate = DateUtil.now();
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm:ss");
+        DateTime fromDate = formatter.parseDateTime(args[0]);
+        DateTime toDate = formatter.parseDateTime(args[1]);
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-tool.xml");
         AllSynchronisers allSynchronisers = (AllSynchronisers) context.getBean("allSynchronisers");
         allSynchronisers.run(fromDate, toDate);
