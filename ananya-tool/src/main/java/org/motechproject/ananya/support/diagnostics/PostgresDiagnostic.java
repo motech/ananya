1aedf7b9c804af6acd842ca156785d6fd4f2818b
Sneha Koti <snehakot@thoughtworks.com>
2012-05-07 22:08:15 +0530

    Sneha | Ashok | #1640 | updated diagnostics to capture total flws registerd with status info, number of jobaid and cc calls, total and for that day

--- ananya-tool/src/main/java/org/motechproject/ananya/support/diagnostics/PostgresDiagnostic.java
+++ ananya-tool/src/main/java/org/motechproject/ananya/support/diagnostics/PostgresDiagnostic.java
@@ -7,6 +7,8 @@ import org.motechproject.ananya.repository.DataAccessTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Iterator;
+
 @Component
 public class PostgresDiagnostic implements Diagnostic {
 
@@ -23,6 +25,18 @@ public class PostgresDiagnostic implements Diagnostic {
             int flwCount = ((Long) session.createQuery("select count(*) from FrontLineWorkerDimension").uniqueResult()).intValue();
             diagnosticLog.add("Front Line Workers in database : " + flwCount);
 
+            Iterator totalCountByRegisteredStatus;
+            totalCountByRegisteredStatus = (session.createQuery(
+                    "select count(*),status from FrontLineWorkerDimension group by status").list().iterator());
+            diagnosticLog.add("\nFront Line Workers in database registered by registration status are : \n");
+            while (totalCountByRegisteredStatus.hasNext()) {
+                Object[] row = (Object[]) totalCountByRegisteredStatus.next();
+                Long count = (Long) row[0];
+                String status = (String) row[1];
+                diagnosticLog.add(status + ":" + count + "\n");
+
+            }
+
             DateTime today = DateTime.now();
             int registeredTodayCount = ((Long) session.createQuery(
                     "select count(*) from FrontLineWorkerDimension flwd, RegistrationMeasure rm, TimeDimension td" +
@@ -31,7 +45,58 @@ public class PostgresDiagnostic implements Diagnostic {
                             " and td.day = " + today.getDayOfYear() +
                             " and td.month = " + today.getMonthOfYear() +
                             " and td.year = " + today.getYear()).uniqueResult()).intValue();
-            diagnosticLog.add("Front Line Workers registered today are : " + registeredTodayCount);
+            diagnosticLog.add("\nFront Line Workers registered today are : " + registeredTodayCount);
+
+            Iterator totalCountByStatusAndDate;
+            totalCountByStatusAndDate = (session.createQuery(
+                    "select count(*),flwd.status from FrontLineWorkerDimension flwd, RegistrationMeasure rm, TimeDimension td" +
+                            " where flwd.id = rm.frontLineWorkerDimension.id" +
+                            " and td.id = rm.timeDimension.id" +
+                            " and td.day = " + today.getDayOfYear() +
+                            " and td.month = " + today.getMonthOfYear() +
+                            " and td.year = " + today.getYear() +
+                            " group by flwd.status")).list().iterator();
+            diagnosticLog.add("\nFront Line Workers registered today by registration status are : \n");
+            while (totalCountByStatusAndDate.hasNext()) {
+                Object[] row = (Object[]) totalCountByStatusAndDate.next();
+                Long count = (Long) row[0];
+                String status = (String) row[1];
+                diagnosticLog.add(status + ":" + count + "\n");
+            }
+
+            int jobAidCallCount = ((Long) session.createQuery("select count(distinct callId) from JobAidContentMeasure").uniqueResult()).intValue();
+            diagnosticLog.add("\nTotal calls made to JobAid : " + jobAidCallCount);
+
+            int jobAidCallCountForToday = ((Long) session.createQuery("select count(distinct jacm.callId) from JobAidContentMeasure jacm, TimeDimension td" +
+                    " where td.id = jacm.timeDimension.id" +
+                    " and td.day = " + today.getDayOfYear() +
+                    " and td.month = " + today.getMonthOfYear() +
+                    " and td.year = " + today.getYear()).uniqueResult()).intValue();
+            diagnosticLog.add("\nTotal calls made today to JobAid : " + jobAidCallCountForToday);
+
+            int certificateCourseCallCount = ((Long) session.createQuery("select count(distinct callId) from CourseItemMeasure").uniqueResult()).intValue();
+            diagnosticLog.add("\nTotal calls made to Certificate Course : " + certificateCourseCallCount);
+
+            int certificateCourseCallCountForToday = ((Long) session.createQuery("select count(distinct cim.callId) from CourseItemMeasure cim, TimeDimension td" +
+                    " where td.id = cim.timeDimension.id" +
+                    " and td.day = " + today.getDayOfYear() +
+                    " and td.month = " + today.getMonthOfYear() +
+                    " and td.year = " + today.getYear()).uniqueResult()).intValue();
+            diagnosticLog.add("\nTotal calls made today to Certificate Course : " + certificateCourseCallCountForToday);
+
+            int smsTotalCount =  ((Long) session.createQuery("select count(*) from SMSSentMeasure " +
+                    "where smsSent = true").uniqueResult()).intValue();
+            diagnosticLog.add("\nTotal SMS sent : " + smsTotalCount);
+
+
+            int smsTotalCountForToday =  ((Long) session.createQuery("select count(*) from SMSSentMeasure ssm, TimeDimension td" +
+                    " where ssm.smsSent = true" +
+                    " and td.id = ssm.timeDimension.id" +
+                    " and td.day = " + today.getDayOfYear() +
+                    " and td.month = " + today.getMonthOfYear() +
+                    " and td.year = " + today.getYear()).uniqueResult()).intValue();
+            diagnosticLog.add("\nTotal SMS sent today: " + smsTotalCountForToday);
+
 
         } catch (Exception e) {
             diagnosticLog.add("Opening session failed");
