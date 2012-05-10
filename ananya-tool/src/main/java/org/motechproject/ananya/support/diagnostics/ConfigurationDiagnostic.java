a19ebd81c8d27fbc8f68427b53038d76b1af2e09
vijayaraj <vijayaraj@yahoo.com>
2012-05-10 22:40:37 +0530

    vijay|#1636|added configuration diagnostics

--- ananya-tool/src/main/java/org/motechproject/ananya/support/diagnostics/ConfigurationDiagnostic.java
+++ ananya-tool/src/main/java/org/motechproject/ananya/support/diagnostics/ConfigurationDiagnostic.java
@@ -1,15 +1,44 @@
 package org.motechproject.ananya.support.diagnostics;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import javax.jms.JMSException;
+import java.util.Properties;
+import java.util.TreeSet;
 
 @Service
 public class ConfigurationDiagnostic implements Diagnostic {
 
+    @Autowired
+    @Qualifier("ananyaProperties")
+    private Properties ananyaProperties;
+
+    @Autowired
+    @Qualifier("couchdbProperties")
+    private Properties couchdbProperties;
+
+    @Autowired
+    @Qualifier("activemqProperties")
+    private Properties activemqProperties;
+
     @Override
     public DiagnosticLog performDiagnosis() throws JMSException {
         DiagnosticLog diagnosticLog = new DiagnosticLog("CONFIGURATION");
+
+        logPropertiesFileFor(diagnosticLog, "ananya.properties", ananyaProperties);
+        logPropertiesFileFor(diagnosticLog, "couchdb.properties", couchdbProperties);
+        logPropertiesFileFor(diagnosticLog, "activemq.properties", activemqProperties);
+
         return diagnosticLog;
+    }
+
+    private void logPropertiesFileFor(DiagnosticLog diagnosticLog, String file, Properties properties) {
+        diagnosticLog.add(file + ":\n");
+        TreeSet sortedKeys = new TreeSet(properties.keySet());
+        for (Object key : sortedKeys)
+            diagnosticLog.add(key + "=" + properties.get(key));
+        diagnosticLog.add("______________________________________________________________");
     }
 }
