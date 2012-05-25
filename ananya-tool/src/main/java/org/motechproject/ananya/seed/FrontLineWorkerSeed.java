1da699e5c1e2473f6bff80049830357d8d25ae36
Sneha Koti <snehakot@thoughtworks.com>
2012-05-25 15:55:53 +0530

    Sneha,Sush | #33 | added comment on AWW seed to be run for updating the designations on postgres

--- ananya-tool/src/main/java/org/motechproject/ananya/seed/FrontLineWorkerSeed.java
+++ ananya-tool/src/main/java/org/motechproject/ananya/seed/FrontLineWorkerSeed.java
@@ -89,7 +89,7 @@ public class FrontLineWorkerSeed {
         seedService.updateOperatorDesignationCircleAndCorrectMsisdnInPostgresAndCouchDb(allFrontLineWorkers, defaultCircle);
     }
 
-    @Seed(priority = 4, version = "1.1", comment = "Correction of invalid status for AWW [P+C]")
+    @Seed(priority = 4, version = "1.3", comment = "Correction of invalid status for AWW [P+C]. If getting rid of designation from couchdb, updated only the postgres entries.")
     public void correctInvalidDesignationsForAnganwadi() throws IOException {
 
         String inputCSVFile = environment.equals("prod") ? inputFileName : getClass().getResource(inputFileName).getPath();
