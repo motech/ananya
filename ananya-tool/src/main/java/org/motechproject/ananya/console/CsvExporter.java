b3ac2b21fd7bb7650007c93e69316a880b76ef88
Vignesh R E <vignesh.littlemaster@gmail.com>
2012-05-25 16:51:18 +0530

    V2, Sharvani | #150 | Made csv export output file name optional

--- ananya-tool/src/main/java/org/motechproject/ananya/console/CsvExporter.java
+++ ananya-tool/src/main/java/org/motechproject/ananya/console/CsvExporter.java
@@ -6,11 +6,9 @@ import org.motechproject.export.model.AllReportDataSources;
 import org.motechproject.export.model.ReportDataSource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.Console;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,7 +17,7 @@ public class CsvExporter {
 
     public static final String APPLICATION_CONTEXT_XML = "applicationContext-tool.xml";
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML);
         AllReportDataSources allReportDataSources = (AllReportDataSources) applicationContext.getBean("allReportDataSources");
         ReportDataSource reportDataSource = allReportDataSources.get(args[0].toUpperCase());
@@ -27,14 +25,24 @@ public class CsvExporter {
             System.out.println("Report Data Source not found: enter a valid value for the first argument..eg: FLW, LOCATION");
         Map<String, String> criteria = new HashMap<String, String>();
 
-        FileInputStream fileInputStream = new FileInputStream(args[1]);
-        List<String> strings = IOUtils.readLines(fileInputStream);
+        FileInputStream fileInputStream = null;
+        List<String> strings = new ArrayList<String>();
+        try {
+            fileInputStream = new FileInputStream(args[1]);
+            strings = IOUtils.readLines(fileInputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         for (String line : strings) {
             String[] keyValue = line.split("=");
             criteria.put(keyValue[0].toLowerCase(), keyValue[1]);
         }
 
-        new CsvReportBuilder(args[2], "queryReport", reportDataSource, criteria).build();
+        String outputFileName = null;
+        if(args.length == 3)
+            outputFileName = args[2];
+
+        new CsvReportBuilder(outputFileName, "queryReport", reportDataSource, criteria).build();
         System.out.println("Report file generated successfully.");
         applicationContext.close();
     }
