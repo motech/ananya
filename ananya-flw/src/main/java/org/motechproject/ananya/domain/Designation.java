f888ce19d3294702f752be038ad42af4a5d6679e
Aravind M <arav.psgtech@gmail.com>
2012-06-01 12:32:45 +0530

    Aravind | #00 | Handling spaces and case insensitivity in designation.

--- ananya-flw/src/main/java/org/motechproject/ananya/domain/Designation.java
+++ ananya-flw/src/main/java/org/motechproject/ananya/domain/Designation.java
@@ -1,25 +1,27 @@
 package org.motechproject.ananya.domain;
 
+import org.apache.commons.lang.StringUtils;
+
 public enum Designation {
     ANM,
     ASHA,
     AWW,
     INVALID;
 
+    public static boolean isInValid(String designation) {
+        return designation == null || !Designation.contains(designation);
+    }
+
+    public static Designation getFor(String designation) {
+        return Designation.isInValid(designation) ? Designation.INVALID : Designation.valueOf(StringUtils.trimToEmpty(designation).toUpperCase());
+    }
+
     private static boolean contains(String value) {
         for (Designation designation : Designation.values()) {
-            if (designation.name().equals(value)) {
+            if (designation.name().equals(StringUtils.trimToEmpty(value).toUpperCase())) {
                 return true;
             }
         }
         return false;
-    }
-
-    public static boolean isInValid(String designation) {
-        return designation == null || !Designation.contains(designation);
-    }
-    
-    public static Designation getFor(String designation) {
-        return Designation.isInValid(designation) ? Designation.INVALID : Designation.valueOf(designation);
     }
 }
