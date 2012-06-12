074c3a352943213a822084f7556321d5e4a27192
Vijay Chakravarthy <vijayaraj210@yahoo.com>
2012-06-12 17:35:24 +0530

    pulkit, vijay|#00|formatting CertificateCourseDataCorrectionSeed

--- ananya-tool/src/main/java/org/motechproject/ananya/seed/CertificateCourseDataCorrectionSeed.java
+++ ananya-tool/src/main/java/org/motechproject/ananya/seed/CertificateCourseDataCorrectionSeed.java
@@ -8,10 +8,7 @@ import org.motechproject.ananya.repository.AllFrontLineWorkers;
 import org.motechproject.ananya.seed.service.CourseItemMeasureSeedService;
 import org.motechproject.ananya.service.SendSMSService;
 import org.motechproject.deliverytools.seed.Seed;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -19,112 +16,26 @@ import java.util.*;
 @Service
 public class CertificateCourseDataCorrectionSeed {
 
-    class ChapterScore {
-
-        private String chapterIndex;
-        private int score;
-
-        ChapterScore(String chapterIndex, int score) {
-            this.chapterIndex = chapterIndex;
-            this.score = score;
-        }
-
-        public String getChapterIndex() {
-            return chapterIndex;
-        }
-
-        public void setChapterIndex(String chapterIndex) {
-            this.chapterIndex = chapterIndex;
-        }
-
-        public int getScore() {
-            return score;
-        }
-
-        public void setScore(int score) {
-            this.score = score;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            ChapterScore that = (ChapterScore) o;
-
-            if (chapterIndex != null ? !chapterIndex.equals(that.chapterIndex) : that.chapterIndex != null)
-                return false;
-
-            return true;
-        }
-
-        @Override
-        public int hashCode() {
-            return chapterIndex != null ? chapterIndex.hashCode() : 0;
-        }
-    }
-
-    class QuizInformation
-    {
-        private String chapterIndex;
-
-        private String type;
-
-        private int score;
-
-        private DateTime timestamp;
-
-        public QuizInformation(Object[] row) {
-            chapterIndex = getChapterIndexFromCourseItemDimension((String)row[1]).toString();
-            type = (String)row[3];
-            if (row[4] == null) score = -1; else score = (Integer)row[4];
-//            if (row[5] != null) timestamp = DateTime.parse(row[5].toString());
-        }
-    }
-
-    private static Logger log = LoggerFactory.getLogger(CertificateCourseDataCorrectionSeed.class);
+    private DateTime endDate = new DateTime(2012, 06, 2, 0, 0);
+    private DateTime startDate = new DateTime(2012, 05, 23, 0, 0);
+    private Map<String, List<QuizInformation>> flwQuizHistoryMap = new HashMap<String, List<QuizInformation>>();
 
     @Autowired
     private CourseItemMeasureSeedService courseItemMeasureSeedService;
-
     @Autowired
     private AllFrontLineWorkers allFrontLineWorkers;
-
     @Autowired
     private SendSMSService sendSMSService;
 
-    private DateTime startDate = new DateTime(2012, 05, 23, 0, 0);
-    private DateTime endDate = new DateTime(2012, 06, 2, 0, 0);
-    private int startTimeId = 144;
-    private int endTimeId = 153;
-
-    private Map<String, List<Integer>> flwChaptersMap = new HashMap<String, List<Integer>>();
-
-    private Map<String, List<ChapterScore>> flwScoresMap = new HashMap<String, List<ChapterScore>>();
-
-    private Map<String, List<QuizInformation>> flwQuizHistoryMap = new HashMap<String, List<QuizInformation>>();
-
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-tool.xml");
-        CertificateCourseDataCorrectionSeed certificateCourseDataCorrectionSeed =
-                (CertificateCourseDataCorrectionSeed) context.getBean("certificateCourseDataCorrectionSeed");
-        certificateCourseDataCorrectionSeed.correctFlwScoresData();
-    }
-
-    private Integer getChapterIndexFromCourseItemDimension(String str) {
-        return Integer.parseInt(str.substring(8, 9)) - 1;
-    }
-
     @Seed(priority = 0, version = "1.4", comment = "Correct incorrect scores data for certificate course.")
     public void correctFlwScoresData() {
-        System.out.println("Correction FLW Scores data.");
+        print("Correction FLW Scores data:START");
 
-        System.out.println("Fetching course ite measure history.");
         List flwCourseHistory = courseItemMeasureSeedService.fetchFlwCourseHistory();
+        print("Fetched course item measure history.");
 
-        System.out.println("Creating FLW QUIZ history map");
         Iterator iterator = flwCourseHistory.iterator();
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             Object[] row = (Object[]) iterator.next();
             String msisdn = row[0].toString();
             QuizInformation quizInformation = new QuizInformation(row);
@@ -137,17 +48,15 @@ public class CertificateCourseDataCorrectionSeed {
                 flwQuizHistoryMap.put(msisdn, quizInformationList);
             }
         }
+        print("Created Flw quiz history map");
 
         for (String msisdn : flwQuizHistoryMap.keySet()) {
-            System.out.println("Correcting the score for FLW with msisdn : " + msisdn);
-
+            print("Correcting the score for FLW with msisdn : " + msisdn);
             FrontLineWorker frontLineWorker = allFrontLineWorkers.findByMsisdn(msisdn);
             List<QuizInformation> quizInformationList = flwQuizHistoryMap.get(msisdn);
-
             ReportCard reportCard = new ReportCard();
-            ReportCard oldReportCard = null;
-
             boolean modified = false;
+
             int oldScore = frontLineWorker.reportCard().totalScore();
             List<Score> scores = frontLineWorker.reportCard().scores();
             for (Score score : scores) {
@@ -157,7 +66,7 @@ public class CertificateCourseDataCorrectionSeed {
 
                 DateTime timeOfCall = new DateTime(Long.parseLong(callIdTime));
                 if (timeOfCall.getMillis() >= startDate.getMillis() &&
-                    timeOfCall.getMillis() < endDate.getMillis()) {
+                        timeOfCall.getMillis() < endDate.getMillis()) {
                     score.setResult(!score.result());
                     modified = true;
                 }
@@ -165,47 +74,67 @@ public class CertificateCourseDataCorrectionSeed {
             int newScore = frontLineWorker.reportCard().totalScore();
 
             if (modified) {
-                System.out.println("Updating FLW : " + msisdn + " oldScore - " + oldScore + " newScore - " + newScore);
+                print("Updating FLW : " + msisdn + " oldScore - " + oldScore + " newScore - " + newScore);
                 allFrontLineWorkers.update(frontLineWorker);
             }
 
             if (frontLineWorker.currentCourseAttempt() > 0) {
-                System.out.println("Checking FLW " + msisdn + " who has completed the course.");
+                print("Checking FLW " + msisdn + " who has completed the course.");
                 int previousChapterIndex = -1;
                 int counter = 0;
                 for (QuizInformation quizInformation : quizInformationList) {
                     counter++;
                     if (quizInformation.type.equalsIgnoreCase("END")) {
                         reportCard.clearScoresForChapterIndex(quizInformation.chapterIndex);
-                        for (int i = 0; i < quizInformation.score; i++) {
+                        for (int i = 0; i < quizInformation.score; i++)
                             reportCard.addScore(new Score(quizInformation.chapterIndex, "" + i, true, ""));
-                        }
-                        for (int i = quizInformation.score; i < 4; i++) {
+
+                        for (int i = quizInformation.score; i < 4; i++)
                             reportCard.addScore(new Score(quizInformation.chapterIndex, "" + i, false, ""));
-                        }
+
                     }
                     if (Integer.parseInt(quizInformation.chapterIndex) < previousChapterIndex ||
                             (quizInformation.chapterIndex.equalsIgnoreCase("8") &&
-                             quizInformation.type.equalsIgnoreCase("END") &&
-                             counter == quizInformationList.size())) {
-                        System.out.println("Switching attempts at " + quizInformation.chapterIndex + " " +
-                                quizInformation.type + " previousChapterIndex is " + previousChapterIndex +
+                                    quizInformation.type.equalsIgnoreCase("END") &&
+                                    counter == quizInformationList.size())) {
+                        print("Switching attempts at " + quizInformation.chapterIndex + " " + quizInformation.type +
+                                " previousChapterIndex is " + previousChapterIndex +
                                 " FLW msisdn is : " + msisdn);
                         if (reportCard.totalScore() > FrontLineWorker.CERTIFICATE_COURSE_PASSING_SCORE) {
-                            System.out.println("FLW " + msisdn + "had passed course. Sending FLW an SMS");
+                            print("FLW " + msisdn + "had passed course. Sending FLW an SMS");
                             sendSMSService.buildAndSendSMS(
                                     frontLineWorker.getMsisdn(),
                                     frontLineWorker.getLocationId(),
                                     frontLineWorker.currentCourseAttempt());
                         }
-
-                        oldReportCard = reportCard;
                         reportCard = new ReportCard();
                     }
                     previousChapterIndex = Integer.parseInt(quizInformation.chapterIndex);
                 }
             }
         }
-
+        print("Correction FLW Scores data:END");
     }
+
+    private void print(String message) {
+        System.out.println(message);
+    }
+
+    private Integer getChapterIndexFromCourseItemDimension(String str) {
+        return Integer.parseInt(str.substring(8, 9)) - 1;
+    }
+
+    class QuizInformation {
+        private String chapterIndex;
+        private String type;
+        private int score;
+
+        public QuizInformation(Object[] row) {
+            chapterIndex = getChapterIndexFromCourseItemDimension((String) row[1]).toString();
+            type = (String) row[3];
+            if (row[4] == null) score = -1;
+            else score = (Integer) row[4];
+        }
+    }
+
 }
