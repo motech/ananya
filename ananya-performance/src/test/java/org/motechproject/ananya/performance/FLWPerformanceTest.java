84088b0c6d7947fe17d816a3717e84a790b724a3
Sharvani <sharvani.n.s@gmail.com>
2012-06-28 15:08:04 +0530

    Vandana, Sharvani |#162| Adding performance tests and corresponding csv.

---
+++ ananya-performance/src/test/java/org/motechproject/ananya/performance/FLWPerformanceTest.java
@@ -0,0 +1,131 @@
+package org.motechproject.ananya.performance;
+
+import org.apache.commons.lang.time.StopWatch;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Projections;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.motechproject.ananya.domain.dimension.FrontLineWorkerDimension;
+import org.motechproject.ananya.domain.dimension.LocationDimension;
+import org.motechproject.ananya.importer.csv.CsvImporter;
+import org.motechproject.ananya.repository.AllFrontLineWorkers;
+import org.motechproject.ananya.repository.AllLocations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.net.URL;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
+@Ignore
+public class FLWPerformanceTest extends SpringIntegrationTest {
+
+    @Autowired
+    private AllLocations allLocations;
+
+    @Autowired
+    private AllFrontLineWorkers allFrontLineWorkers;
+
+    private static DetachedCriteria criteria;
+    private static boolean locationLoaded = false;
+    private int numberOfFlwsToBeCreated;
+
+    public void createAllLocations() throws Exception {
+        if (!locationLoaded) {
+            template.deleteAll(template.loadAll(LocationDimension.class));
+            allLocations.removeAll();
+            URL locationData = this.getClass().getResource("/locations_5000.csv");
+            LocationPerformanceTest.loadLocationData(locationData, 5000);
+            locationLoaded = true;
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        template.deleteAll(template.loadAll(FrontLineWorkerDimension.class));
+        allFrontLineWorkers.removeAll();
+        createAllLocations();
+        criteria = DetachedCriteria.forClass(FrontLineWorkerDimension.class);
+        criteria.setProjection(Projections.rowCount());
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        template.deleteAll(template.loadAll(FrontLineWorkerDimension.class));
+        template.deleteAll(template.loadAll(LocationDimension.class));
+    }
+
+    @Test
+    public void shouldImportFLWDataFor100Records() throws Exception {
+        URL flwData = this.getClass().getResource("/flws_100.csv");
+        numberOfFlwsToBeCreated = 100;
+        loadFLWData(flwData, numberOfFlwsToBeCreated);
+        assertNumberOfFlwsCreated(numberOfFlwsToBeCreated);
+    }
+
+    @Test
+    public void shouldImportFLWDataFor500Records() throws Exception {
+        URL flwData = this.getClass().getResource("/flws_500.csv");
+        numberOfFlwsToBeCreated = 500;
+        loadFLWData(flwData, numberOfFlwsToBeCreated);
+        assertNumberOfFlwsCreated(numberOfFlwsToBeCreated);
+    }
+
+    @Test
+    public void shouldImportFLWDataFor1000Records() throws Exception {
+        URL flwData = this.getClass().getResource("/flws_1000.csv");
+        numberOfFlwsToBeCreated = 1000;
+        loadFLWData(flwData, numberOfFlwsToBeCreated);
+        assertNumberOfFlwsCreated(numberOfFlwsToBeCreated);
+    }
+
+    @Test
+    public void shouldImportFLWDataFor5000Records() throws Exception {
+        URL flwData = this.getClass().getResource("/flws_5000.csv");
+        numberOfFlwsToBeCreated = 5000;
+        loadFLWData(flwData, numberOfFlwsToBeCreated);
+        assertNumberOfFlwsCreated(numberOfFlwsToBeCreated);
+    }
+
+    @Test
+    public void shouldImportFLWDataFor10000Records() throws Exception {
+        URL flwData = this.getClass().getResource("/flws_10000.csv");
+        numberOfFlwsToBeCreated = 10000;
+        loadFLWData(flwData, numberOfFlwsToBeCreated);
+        assertNumberOfFlwsCreated(numberOfFlwsToBeCreated);
+    }
+
+    @Test
+    public void shouldImportFLWDataFor25000Records() throws Exception {
+        URL flwData = this.getClass().getResource("/flws_25000.csv");
+        numberOfFlwsToBeCreated = 25000;
+        loadFLWData(flwData, numberOfFlwsToBeCreated);
+        assertNumberOfFlwsCreated(numberOfFlwsToBeCreated);
+    }
+
+    @Test
+    public void shouldImportFLWDataFor50000Records() throws Exception {
+        URL flwData = this.getClass().getResource("/flws_50000.csv");
+        numberOfFlwsToBeCreated = 50000;
+        loadFLWData(flwData, numberOfFlwsToBeCreated);
+        assertNumberOfFlwsCreated(numberOfFlwsToBeCreated);
+    }
+
+    private void loadFLWData(URL flwData, int count) throws Exception {
+        String[] arguments = {"FrontLineWorker", flwData.getPath()};
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        CsvImporter.main(arguments);
+
+        stopWatch.stop();
+        System.out.println("Total time to load " + count + "flw : " + stopWatch.getTime() + "ms");
+    }
+
+    private void assertNumberOfFlwsCreated(int expectedNumberOfFlws) {
+        assertEquals(expectedNumberOfFlws, template.loadAll(FrontLineWorkerDimension.class).size());
+        assertEquals(expectedNumberOfFlws, allFrontLineWorkers.getAll().size());
+    }
+}
---
+++ ananya-performance/src/test/java/org/motechproject/ananya/performance/LocationPerformanceTest.java
@@ -0,0 +1,85 @@
+package org.motechproject.ananya.performance;
+
+import org.apache.commons.lang.time.StopWatch;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.motechproject.ananya.domain.dimension.LocationDimension;
+import org.motechproject.ananya.importer.csv.CsvImporter;
+import org.motechproject.ananya.repository.AllLocations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.net.URL;
+
+import static junit.framework.Assert.assertEquals;
+
+@Ignore
+public class LocationPerformanceTest extends SpringIntegrationTest {
+
+    @Autowired
+    private AllLocations allLocations;
+
+    @Before
+    @After
+    public void setUp() {
+        template.deleteAll(template.loadAll(LocationDimension.class));
+        allLocations.removeAll();
+    }
+
+    @Test
+    public void shouldImportLocationDataFor100Records() throws Exception {
+        URL locationData = this.getClass().getResource("/locations_100.csv");
+        int locationCount = 100;
+        loadLocationData(locationData, locationCount);
+        assertLocations(locationCount);
+    }
+
+    @Test
+    public void shouldImportLocationDataFor500Records() throws Exception {
+        URL locationData = this.getClass().getResource("/locations_500.csv");
+        int locationCount = 500;
+        loadLocationData(locationData, locationCount);
+        assertLocations(locationCount);
+    }
+
+    @Test
+    public void shouldImportLocationDataFor1000Records() throws Exception {
+        URL locationData = this.getClass().getResource("/locations_1000.csv");
+        int locationCount = 1000;
+        loadLocationData(locationData, locationCount);
+        assertLocations(locationCount);
+    }
+
+    @Test
+    public void shouldImportLocationDataFor2500Records() throws Exception {
+        URL locationData = this.getClass().getResource("/locations_2500.csv");
+        int locationCount = 2500;
+        loadLocationData(locationData, locationCount);
+        assertLocations(locationCount);
+    }
+
+    @Test
+    public void shouldImportLocationDataFor5000Records() throws Exception {
+        URL locationData = this.getClass().getResource("/locations_5000.csv");
+        int locationCount = 5000;
+        loadLocationData(locationData, locationCount);
+        assertLocations(locationCount);
+    }
+
+    public static void loadLocationData(URL locationData, int count) throws Exception {
+        String[] arguments = {"Location", locationData.getPath()};
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        CsvImporter.main(arguments);
+
+        stopWatch.stop();
+        System.out.println("Total time to load " + count + "locations : " + stopWatch.getTime() + "ms");
+    }
+
+    private void assertLocations(int locationCount) {
+        assertEquals(locationCount, allLocations.getAll().size());
+        assertEquals(locationCount, template.loadAll(LocationDimension.class).size());
+    }
+}
