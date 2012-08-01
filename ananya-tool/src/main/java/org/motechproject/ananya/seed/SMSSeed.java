deb5383a981a25e4c55d013eca71f28d812cc3d0
Vijay Chakravarthy <vijayaraj210@yahoo.com>
2012-08-01 18:59:18 +0530

    sush, vijay|#177|added seed to send sms to one angry lady

---
+++ ananya-tool/src/main/java/org/motechproject/ananya/seed/SMSSeed.java
@@ -0,0 +1,34 @@
+package org.motechproject.ananya.seed;
+
+import org.motechproject.ananya.domain.FrontLineWorker;
+import org.motechproject.ananya.repository.AllFrontLineWorkers;
+import org.motechproject.ananya.seed.service.SMSSeedService;
+import org.motechproject.deliverytools.seed.Seed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SMSSeed {
+
+    private static final Logger log = LoggerFactory.getLogger(SMSSeed.class);
+
+    @Autowired
+    private SMSSeedService smsSeedService;
+
+    @Autowired
+    private AllFrontLineWorkers allFrontLineWorkers;
+
+    @Seed(priority = 1, version = "1.6", comment = "sending sms to frontlineWorker who had completed course but smsLog got purged during migration: Refer bug #177 ")
+    public void sendSMSForCourseCompletedFrontlineWorkers() {
+        String callerId = "919931004287";
+        FrontLineWorker frontLineWorker = allFrontLineWorkers.findByMsisdn(callerId);
+        if (frontLineWorker == null) {
+            log.info("FLW not present to send SMS:" + callerId);
+            return;
+        }
+        smsSeedService.buildAndSendSMS(frontLineWorker.getMsisdn(), frontLineWorker.getLocationId(), 0);
+        log.info("Sent SMS for:" + callerId);
+    }
+}
