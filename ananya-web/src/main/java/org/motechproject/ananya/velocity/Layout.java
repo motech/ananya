09d140c3a0cd42edfa99713d58ff876a1bd6ee6a
Vijay Chakravarthy <vijayaraj210@yahoo.com>
2012-08-17 18:53:07 +0530

    sush, vijay|#136| added more urls to Layout - admin console

--- ananya-web/src/main/java/org/motechproject/ananya/velocity/Layout.java
+++ ananya-web/src/main/java/org/motechproject/ananya/velocity/Layout.java
@@ -7,10 +7,12 @@ public class Layout {
     private static Map<String, String> map = new LinkedHashMap<String, String>();
 
     static {
-        map.put("/admin/monitor", "layout/admin-default.vm");
-        map.put("/admin/inquiry", "layout/admin-default.vm");
         map.put("/admin/login", "layout/admin-login.vm");
         map.put("/admin/logs", "layout/admin-default.vm");
+        map.put("/admin/monitor", "layout/admin-default.vm");
+        map.put("/admin/inquiry", "layout/admin-default.vm");
+        map.put("/internal/admin/monitor", "layout/admin-default.vm");
+        map.put("/internal/admin/logs", "layout/admin-default.vm");
     }
 
     public static String get(String path) {
