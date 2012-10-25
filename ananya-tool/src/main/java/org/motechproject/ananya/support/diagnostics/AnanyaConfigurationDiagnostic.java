91a85cf864784fd3a29162f5f943d80a2f47c979
f1codz <imdad.ahmed@gmail.com>
2012-10-25 15:40:09 +0530

    Imdad | #349 | Picking actual configuration values for couchdb and activemq in ananya diagnose

--- ananya-tool/src/main/java/org/motechproject/ananya/support/diagnostics/AnanyaConfigurationDiagnostic.java
+++ ananya-tool/src/main/java/org/motechproject/ananya/support/diagnostics/AnanyaConfigurationDiagnostic.java
@@ -1,13 +1,22 @@
 package org.motechproject.ananya.support.diagnostics;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.ektorp.impl.StdCouchDbInstance;
+import org.ektorp.spring.HttpClientFactoryBean;
 import org.motechproject.diagnostics.annotation.Diagnostic;
 import org.motechproject.diagnostics.diagnostics.DiagnosticLog;
 import org.motechproject.diagnostics.response.DiagnosticsResult;
+import org.motechproject.event.MotechEventConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.stereotype.Component;
 
+import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
+import javax.jms.Queue;
+import java.lang.reflect.Field;
 import java.util.Properties;
 import java.util.TreeSet;
 
@@ -17,14 +26,29 @@ public class AnanyaConfigurationDiagnostic {
     private Properties ananyaProperties;
     private Properties couchdbProperties;
     private Properties activemqProperties;
+    private HttpClientFactoryBean httpClient;
+    private CachingConnectionFactory connectionFactory;
+    private Queue eventQueue;
+    private Queue schedulerQueue;
+    private MotechEventConfig motechEventConfig;
 
     @Autowired
     public AnanyaConfigurationDiagnostic(@Qualifier("ananyaProperties") Properties ananyaProperties,
                                          @Qualifier("couchdbProperties") Properties couchdbProperties,
-                                         @Qualifier("activemqProperties") Properties activemqProperties) {
+                                         @Qualifier("activemqProperties") Properties activemqProperties,
+                                         @Qualifier("httpClient") HttpClientFactoryBean httpClient,
+                                         CachingConnectionFactory connectionFactory,
+                                         Queue eventQueue,
+                                         Queue schedulerQueue,
+                                         MotechEventConfig motechEventConfig) {
         this.ananyaProperties = ananyaProperties;
         this.couchdbProperties = couchdbProperties;
         this.activemqProperties = activemqProperties;
+        this.httpClient = httpClient;
+        this.connectionFactory = connectionFactory;
+        this.eventQueue = eventQueue;
+        this.schedulerQueue = schedulerQueue;
+        this.motechEventConfig = motechEventConfig;
     }
 
     @Diagnostic(name = "ananyaConfiguration")
@@ -32,8 +56,8 @@ public class AnanyaConfigurationDiagnostic {
         DiagnosticLog diagnosticLog = new DiagnosticLog();
 
         logPropertiesFileFor(diagnosticLog, "ananya.properties", ananyaProperties);
-        logPropertiesFileFor(diagnosticLog, "couchdb.properties", couchdbProperties);
-        logPropertiesFileFor(diagnosticLog, "activemq.properties", activemqProperties);
+        logPropertiesForCouchDb(diagnosticLog);
+        logPropertiesForActiveMQ(diagnosticLog);
 
         return new DiagnosticsResult(true, diagnosticLog.toString());
     }
@@ -43,6 +67,49 @@ public class AnanyaConfigurationDiagnostic {
         TreeSet sortedKeys = new TreeSet(properties.keySet());
         for (Object key : sortedKeys)
             diagnosticLog.add(key + "=" + properties.get(key));
+        diagnosticLog.add("______________________________________________________________");
+    }
+
+    private void logPropertiesForCouchDb(DiagnosticLog diagnosticLog) {
+        diagnosticLog.add("\nFrom couchdb.properties file:");
+        diagnosticLog.add("-----------------------------");
+        TreeSet sortedKeys = new TreeSet(couchdbProperties.keySet());
+        for (Object key : sortedKeys)
+            diagnosticLog.add(key + "=" + couchdbProperties.get(key));
+
+        diagnosticLog.add("\nActual values:");
+        diagnosticLog.add("---------------");
+        try {
+            for (Object key : sortedKeys) {
+                Field keyField = HttpClientFactoryBean.class.getDeclaredField((String) key);
+                keyField.setAccessible(true);
+                diagnosticLog.add(key + "=" + keyField.get(httpClient));
+            }
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+        diagnosticLog.add("______________________________________________________________");
+    }
+
+    private void logPropertiesForActiveMQ(DiagnosticLog diagnosticLog) throws JMSException {
+        diagnosticLog.add("\nFrom activemq.properties file:");
+        diagnosticLog.add("------------------------------");
+        TreeSet sortedKeys = new TreeSet(activemqProperties.keySet());
+        for (Object key : sortedKeys)
+            diagnosticLog.add(key + "=" + activemqProperties.get(key));
+
+        diagnosticLog.add("\nActual values:");
+        diagnosticLog.add("--------------");
+        diagnosticLog.add("queue.for.events=" + eventQueue.getQueueName());
+        diagnosticLog.add("queue.for.scheduler=" + schedulerQueue.getQueueName());
+        ActiveMQConnectionFactory targetConnectionFactory = (ActiveMQConnectionFactory) connectionFactory.getTargetConnectionFactory();
+        diagnosticLog.add("broker.url=" + targetConnectionFactory.getProperties().get("brokerURL"));
+        diagnosticLog.add("maximumRedeliveries=" + targetConnectionFactory.getRedeliveryPolicy().getMaximumRedeliveries());
+        diagnosticLog.add("redeliveryDelayInMillis=" + targetConnectionFactory.getRedeliveryPolicy().getRedeliveryDelay());
+        diagnosticLog.add("motech.message.max.redelivery.count=" + motechEventConfig.getMessageMaxRedeliveryCount());
+        diagnosticLog.add("motech.message.redelivery.delay=" + motechEventConfig.getMessageRedeliveryDelay());
+
         diagnosticLog.add("______________________________________________________________");
     }
 }
--- ananya-tool/src/test/java/org/motechproject/ananya/support/diagnostics/ConfigurationDiagnosticTest.java
+++ ananya-tool/src/test/java/org/motechproject/ananya/support/diagnostics/ConfigurationDiagnosticTest.java
@@ -1,9 +1,15 @@
 package org.motechproject.ananya.support.diagnostics;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.ektorp.spring.HttpClientFactoryBean;
 import org.junit.Before;
 import org.junit.Test;
+import org.motechproject.event.MotechEventConfig;
+import org.springframework.jms.connection.CachingConnectionFactory;
 
-import javax.jms.JMSException;
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
 import java.util.Properties;
 
 import static junit.framework.Assert.assertTrue;
@@ -15,26 +21,42 @@ public class ConfigurationDiagnosticTest {
     private Properties ananyaProperties = new Properties();
 
     private AnanyaConfigurationDiagnostic diagnostic;
+    private HttpClientFactoryBean httpClient = new HttpClientFactoryBean();
+    private CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+    private Queue eventQueue = new ActiveMQQueue();
+    private Queue schedulerQueue = new ActiveMQQueue();
+    private MotechEventConfig motechEventConfig = new MotechEventConfig();
 
     @Before
     public void setUp() {
-        diagnostic = new AnanyaConfigurationDiagnostic(ananyaProperties, couchdbProperties, activemqProperties);
+        diagnostic = new AnanyaConfigurationDiagnostic(ananyaProperties, couchdbProperties, activemqProperties, httpClient, connectionFactory, eventQueue, schedulerQueue, motechEventConfig);
     }
 
     @Test
-    public void shouldPrintAllPropertiesToLog() throws JMSException {
+    public void shouldPrintAllPropertiesToLog() throws Exception {
         activemqProperties.put("A1", "A1Value");
         activemqProperties.put("A2", "A2Value");
-        couchdbProperties.put("C1", "C1Value");
+
         ananyaProperties.put("AN1", "AN1Value");
         ananyaProperties.put("AN2", "AN2Value");
 
+        couchdbProperties.put("host", "localhost3");
+        httpClient.setProperties(couchdbProperties);
+        httpClient.afterPropertiesSet();
+
+        connectionFactory.setTargetConnectionFactory(new ActiveMQConnectionFactory());
+        ActiveMQConnectionFactory targetConnectionFactory = (ActiveMQConnectionFactory) connectionFactory.getTargetConnectionFactory();
+        targetConnectionFactory.setBrokerURL("tcp://localhost:61616?jms.prefetchPolicy.all=0");
+
+
         String diagnosticLog = diagnostic.performDiagnosis().getMessage();
+
 
         assertTrue(diagnosticLog.contains("AN1=AN1Value"));
         assertTrue(diagnosticLog.contains("AN2=AN2Value"));
         assertTrue(diagnosticLog.contains("A1=A1Value"));
         assertTrue(diagnosticLog.contains("A2=A2Value"));
-        assertTrue(diagnosticLog.contains("C1=C1Value"));
+        assertTrue(diagnosticLog.contains("host=localhost3"));
+        assertTrue(diagnosticLog.contains("broker.url=tcp://localhost:61616"));
     }
 }
