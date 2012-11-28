8480f0bf06ad9d1dffd48f3cb5979a40851066be
sushmitharaos <sushmitharaos@gmail.com>
2012-11-28 11:01:53 +0530

    Sush|#354|added pulse to seconds mapping for bsnl

--- ananya-tool/src/main/java/org/motechproject/ananya/seed/OperatorSeed.java
+++ ananya-tool/src/main/java/org/motechproject/ananya/seed/OperatorSeed.java
@@ -42,8 +42,8 @@ public class OperatorSeed {
         start_of_pulse_map.put("idea", 500);
         start_of_pulse_map.put("reliance", 500);
         start_of_pulse_map.put("airtel", 500);
+        start_of_pulse_map.put("bsnl", 500);
         start_of_pulse_map.put("vodafone", DEFAULT_START_OF_PULSE_IN_MILLI_SEC);
-        start_of_pulse_map.put("bsnl", DEFAULT_START_OF_PULSE_IN_MILLI_SEC);
         start_of_pulse_map.put("undefined", DEFAULT_START_OF_PULSE_IN_MILLI_SEC);
         start_of_pulse_map.put("longcode", DEFAULT_START_OF_PULSE_IN_MILLI_SEC);
 
@@ -51,8 +51,8 @@ public class OperatorSeed {
         end_of_pulse_map.put("idea", 60500);
         end_of_pulse_map.put("reliance", 60500);
         end_of_pulse_map.put("airtel", 60500);
+        end_of_pulse_map.put("bsnl", 60500);
         end_of_pulse_map.put("vodafone", DEFAULT_END_OF_PULSE_IN_MILLI_SEC);
-        end_of_pulse_map.put("bsnl", DEFAULT_END_OF_PULSE_IN_MILLI_SEC);
         end_of_pulse_map.put("undefined", DEFAULT_END_OF_PULSE_IN_MILLI_SEC);
         end_of_pulse_map.put("longcode", DEFAULT_END_OF_PULSE_IN_MILLI_SEC);
     }
