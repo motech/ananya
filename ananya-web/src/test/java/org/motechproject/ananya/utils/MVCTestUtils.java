d01e8e6ddfee1890ab39fb427794c5bbd42ef09a
Ashish Mishra <ashishkm@thoughtworks.com>
2012-12-11 15:41:44 +0530

    Ashish | #105 | Changeing json converter in MockMVC utils.

--- ananya-web/src/test/java/org/motechproject/ananya/utils/MVCTestUtils.java
+++ ananya-web/src/test/java/org/motechproject/ananya/utils/MVCTestUtils.java
@@ -1,6 +1,7 @@
 package org.motechproject.ananya.utils;
 
 import org.motechproject.web.message.converters.CustomJaxb2RootElementHttpMessageConverter;
+import org.motechproject.web.message.converters.CustomMappingJacksonHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.test.web.server.MockMvc;
 import org.springframework.test.web.server.setup.MockMvcBuilders;
@@ -10,7 +11,7 @@ public class MVCTestUtils {
 
     public static MockMvc mockMvc(Object controller) {
         StandaloneMockMvcBuilder mockMvcBuilder = MockMvcBuilders.standaloneSetup(controller);
-        mockMvcBuilder.setMessageConverters(new MappingJacksonHttpMessageConverter(), new CustomJaxb2RootElementHttpMessageConverter());
+        mockMvcBuilder.setMessageConverters(new CustomMappingJacksonHttpMessageConverter(), new CustomJaxb2RootElementHttpMessageConverter());
         return mockMvcBuilder.build();
     }
 }
