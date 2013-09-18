cd2fe8239a29818c992c48ba592ee6971b32d9e8
Mewanbanjop <mewanm@thoughtworks.com>
2013-09-18 20:47:28 +0530

    Vinod, Mewan | #0 | Added migration scripts to migrate the flw_history_id into measure tables

---
+++ ananya-web/src/main/resources/sql/course_item_measure_migration.sql
@@ -0,0 +1,129 @@
+﻿ALTER TABLE report.course_item_measure DROP CONSTRAINT fk_course_item_measure_flw_dimension;
+ALTER TABLE report.course_item_measure DROP CONSTRAINT fk_course_item_measure_location_dimension;
+ALTER TABLE report.course_item_measure DROP CONSTRAINT fk_course_item_measure_time_dimension;
+ALTER TABLE report.course_item_measure DROP CONSTRAINT fk_course_item_measure_course_item_dimension;
+ALTER TABLE report.course_item_measure DROP CONSTRAINT fk_course_item_measure_language_dimension;
+ALTER TABLE report.course_item_measure DROP CONSTRAINT pk_course_item_measure;
+
+DROP INDEX report.course_item_measure_location_id_idx;
+DROP INDEX report.idx_course_item_measure_course_item_id;
+DROP INDEX report.idx_course_item_measure_flw_id;
+DROP INDEX report.idx_course_item_measure_time_id;
+
+ALTER TABLE report.course_item_measure RENAME TO course_item_measure_temp;
+
+CREATE TABLE report.course_item_measure
+(
+  id             INTEGER NOT NULL,
+  flw_id         BIGINT,
+  course_item_id INTEGER,
+  time_id        INTEGER,
+  score          INTEGER,
+  event          CHARACTER VARYING(30),
+  location_id    BIGINT,
+  "timestamp"    TIMESTAMP WITH TIME ZONE,
+  percentage     SMALLINT,
+  call_id        CHARACTER VARYING(255),
+  duration       INTEGER,
+  language_id    INTEGER NOT NULL,
+  flw_history_id INTEGER
+)
+WITH (
+OIDS = FALSE
+);
+
+
+INSERT INTO report.course_item_measure
+(
+  id,
+  flw_id,
+  course_item_id,
+  time_id,
+  score,
+  event,
+  location_id,
+  "timestamp",
+  percentage,
+  call_id,
+  duration,
+  language_id,
+  flw_history_id
+)
+  (
+    SELECT
+      d.id,
+      d.flw_id,
+      d.course_item_id,
+      d.time_id,
+      d.score,
+      d.event,
+      d.location_id,
+      d."timestamp",
+      d.percentage,
+      d.call_id,
+      d.duration,
+      d.language_id,
+      h.id AS flw_history_id
+    FROM
+        report.course_item_measure_temp d
+        JOIN report.front_line_worker_history h
+          ON (h.flw_id = d.flw_id)
+  );
+
+ALTER TABLE report.course_item_measure ADD CONSTRAINT pk_course_item_measure PRIMARY KEY (id);
+
+ALTER TABLE report.course_item_measure
+ADD CONSTRAINT fk_course_item_measure_course_item_dimension FOREIGN KEY (course_item_id)
+REFERENCES report.course_item_dimension (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE CASCADE;
+
+ALTER TABLE report.course_item_measure
+ADD CONSTRAINT fk_course_item_measure_flw_dimension FOREIGN KEY (flw_id)
+REFERENCES report.front_line_worker_dimension (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE CASCADE;
+
+ALTER TABLE report.course_item_measure
+ADD CONSTRAINT fk_course_item_measure_language_dimension FOREIGN KEY (language_id)
+REFERENCES report.language_dimension (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE CASCADE;
+
+ALTER TABLE report.course_item_measure
+ADD CONSTRAINT fk_course_item_measure_location_dimension FOREIGN KEY (location_id)
+REFERENCES report.location_dimension (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE CASCADE;
+
+ALTER TABLE report.course_item_measure
+ADD CONSTRAINT fk_course_item_measure_time_dimension FOREIGN KEY (time_id)
+REFERENCES report.time_dimension (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE CASCADE;
+
+CREATE INDEX course_item_measure_location_id_idx
+ON report.course_item_measure
+USING BTREE
+(location_id);
+
+CREATE INDEX idx_course_item_measure_course_item_id
+ON report.course_item_measure
+USING BTREE
+(course_item_id);
+
+CREATE INDEX idx_course_item_measure_flw_id
+ON report.course_item_measure
+USING BTREE
+(flw_id);
+
+CREATE INDEX idx_course_item_measure_time_id
+ON report.course_item_measure
+USING BTREE
+(time_id);
+
+ALTER SEQUENCE report.course_item_measure_id_seq OWNED BY report.call_duration_measure.id;
+ALTER TABLE report.course_item_measure ALTER COLUMN id SET DEFAULT nextval('report.course_item_measure_id_seq');
+
+ALTER TABLE report.course_item_measure
+OWNER TO postgres;
+GRANT ALL ON TABLE report.course_item_measure TO postgres;
+GRANT SELECT ON TABLE report.course_item_measure TO motech;
+
+
+--DROP TABLE report.course_item_measure_temp;
---
+++ ananya-web/src/main/resources/sql/job_aid_measure_migration.sql
@@ -0,0 +1,93 @@
+﻿
+ALTER TABLE report.job_aid_content_measure DROP CONSTRAINT  fk_ja_content_measure_ja_content_dimension ;
+ALTER TABLE report.job_aid_content_measure DROP CONSTRAINT  fk_job_aid_content_measure_flw_dimension ;
+ALTER TABLE report.job_aid_content_measure DROP CONSTRAINT  fk_job_aid_content_measure_location_dimension ;
+ALTER TABLE report.job_aid_content_measure DROP CONSTRAINT  fk_job_aid_content_measure_time_dimension ;
+ALTER TABLE report.job_aid_content_measure DROP CONSTRAINT  pk_job_aid_content_measure ;
+DROP INDEX report.job_aid_content_measure_location_id_idx;
+
+ALTER TABLE report.job_aid_content_measure RENAME TO job_aid_content_measure_temp;
+
+CREATE TABLE report.job_aid_content_measure
+(
+  id integer NOT NULL,
+  flw_id bigint,
+  call_id character varying(255),
+  location_id bigint,
+  job_aid_content_id integer,
+  "timestamp" timestamp with time zone,
+  percentage smallint,
+  time_id bigint,
+  duration integer,
+  flw_history_id integer
+ )
+WITH (
+  OIDS=FALSE
+);
+
+
+INSERT into report.job_aid_content_measure 
+            (
+        id,
+    flw_id,
+    call_id,
+    location_id,
+    job_aid_content_id,
+    "timestamp",
+    percentage,
+    time_id,
+    duration,
+    flw_history_id
+            )
+            (
+            select
+        d.id,
+    d.flw_id,
+    d.call_id,
+    d.location_id,
+    d.job_aid_content_id,
+    d."timestamp",
+    d.percentage,
+    d.time_id,
+    d.duration,
+    h.id as flw_history_id        
+    from
+            report.job_aid_content_measure_temp d
+            join report.front_line_worker_history h on (h.flw_id = d.flw_id)
+            );	
+
+
+  
+   ALTER TABLE report.job_aid_content_measure ADD CONSTRAINT pk_job_aid_content_measure PRIMARY KEY (id);
+
+   ALTER TABLE report.job_aid_content_measure ADD CONSTRAINT fk_ja_content_measure_ja_content_dimension FOREIGN KEY (job_aid_content_id)
+      REFERENCES report.job_aid_content_dimension (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE;
+
+  ALTER TABLE report.job_aid_content_measure ADD CONSTRAINT fk_job_aid_content_measure_flw_dimension FOREIGN KEY (flw_id)
+      REFERENCES report.front_line_worker_dimension (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE;
+
+  ALTER TABLE report.job_aid_content_measure ADD CONSTRAINT fk_job_aid_content_measure_location_dimension FOREIGN KEY (location_id)
+      REFERENCES report.location_dimension (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE;
+
+  ALTER TABLE report.job_aid_content_measure ADD CONSTRAINT fk_job_aid_content_measure_time_dimension FOREIGN KEY (time_id)
+      REFERENCES report.time_dimension (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE;
+
+  CREATE INDEX job_aid_content_measure_location_id_idx
+	ON report.job_aid_content_measure
+	USING btree
+	(location_id);
+
+ALTER SEQUENCE report.job_aid_content_measure_id_seq OWNED BY report.call_duration_measure.id;
+ALTER TABLE report.job_aid_content_measure ALTER COLUMN id SET DEFAULT nextval('report.job_aid_content_measure_id_seq');
+
+  ALTER TABLE report.job_aid_content_measure
+    OWNER TO postgres;
+    GRANT ALL ON TABLE report.job_aid_content_measure TO postgres;
+    GRANT SELECT ON TABLE report.job_aid_content_measure TO motech;
+
+
+--DROP TABLE report.job_aid_content_measure_temp;
