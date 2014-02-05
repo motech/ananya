f72b47f7eb9fb1be6461e984b0dcc544174dbafb
sharikaep <sharikaep@gmail.com>
2014-02-05 16:38:56 +0530

    <sharika>

--- ananya-flw/src/main/java/org/motechproject/ananya/service/FrontLineWorkerService.java
+++ ananya-flw/src/main/java/org/motechproject/ananya/service/FrontLineWorkerService.java
@@ -73,7 +73,7 @@ public class FrontLineWorkerService {
 
     public FrontLineWorkerCreateResponse createOrUpdateForCall(String callerId, String operator, String circle, String language) {
         FrontLineWorker frontLineWorker = findByCallerId(callerId);
-
+		log.info("frontLineWorker="+frontLineWorker);
         //new flw
         if (frontLineWorker == null) {
             try {
